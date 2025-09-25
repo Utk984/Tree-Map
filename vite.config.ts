555daaedb7f89827ebb65aa9b6034ffdc54a608f
Utk984 <utkarshagarwal984@gmail.com>
2025-09-25 15:37:25 +0530

    Fix GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Tree-Map/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/tree-map.github.io/' : '/',
   server: {
     port: 3000,
     proxy: {
